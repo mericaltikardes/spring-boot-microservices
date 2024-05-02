177163a9b902460aa64fbed6b663b9b3846c21c9
Meric Altikardes <200202088@kocaeli.edu.tr>
2024-05-02 17:01:49 +0300

    react-frontend added, swagger added and customized in department service

--- department-service/src/main/java/com/mericaltikardes/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/mericaltikardes/departmentservice/DepartmentServiceApplication.java
@@ -1,8 +1,23 @@
 package com.mericaltikardes.departmentservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service REST APIs",
+				description = "Department Service REST APIs Doc",
+				version = "V1.0",
+				contact = @Contact(
+						name = "Meriç",
+						email = "mericaltikardes@gmail.com"
+				)
+
+		)
+)
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
--- department-service/src/main/java/com/mericaltikardes/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/mericaltikardes/departmentservice/controller/DepartmentController.java
@@ -3,10 +3,17 @@ package com.mericaltikardes.departmentservice.controller;
 import com.mericaltikardes.departmentservice.dto.DepartmentDto;
 import com.mericaltikardes.departmentservice.service.DepartmentService;
 import com.mericaltikardes.departmentservice.service.Impl.DepartmentServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name="Department Controller",
+        description = "Department Controller"
+)
 @RestController
 @RequestMapping("api/departments")
 public class DepartmentController {
@@ -17,12 +24,27 @@ public class DepartmentController {
         this.departmentService = departmentService;
     }
 
+    @Operation(
+            summary = "Save Department REST API",
+            description = "Save Department REST API is used to save department object in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
         DepartmentDto savedDepartment= departmentService.saveDepartment(departmentDto);
         return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
     }
-
+    @Operation(
+            summary = "Get Department REST API",
+            description = "Get Department REST API is used to save department object in a database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("{code}")
     public DepartmentDto getByDepartmentId(@PathVariable("code") String departmentCode ){
         return departmentService.getDepartmentByCode(departmentCode);
